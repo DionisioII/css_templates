3332bad6a1e6f1725f0b977ab18953fcf0b6dcd1
giorgio aurispa <jaws24@hotmail.it>
2020-11-18 15:06:34 +0100

    upload

---
+++ Dashboard/js/beaviour.js
@@ -0,0 +1,31 @@
+const openMenu = document.querySelector('#show-menu')
+const hideMenuIcon = document.querySelector('#hiding-arrow')
+const sideBar = document.querySelector('#sidebar')
+const userMenuIcon = document.querySelector('#user-menu-icon')
+const userMenu = document.querySelector('#user-menu')
+const notificationMenuIcon = document.querySelector('#notificationIcon')
+const notificationMenu = document.querySelector('#userNotificationDiv')
+
+
+
+hideMenuIcon.addEventListener('click',function(){
+    sideBar.classList.toggle('hidden')
+    
+})
+
+userMenuIcon.addEventListener('click',function(){
+    if (!notificationMenu.classList.contains('hidden')) {
+        notificationMenu.classList.add('hidden')
+        
+    }
+    userMenu.classList.toggle('hidden')
+    
+})
+notificationMenuIcon.addEventListener('click',function(){
+    if (!userMenu.classList.contains('hidden')) {
+        userMenu.classList.add('hidden')
+        
+    }
+    notificationMenu.classList.toggle('hidden')
+    
+})
